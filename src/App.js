6715897600d6ab4976cd4271c0cd337afd555cf5
Shrushti Polekar <shrushtipolekar@gmail.com>
2022-06-07 20:30:07 +0530

    feat: follow user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components";
+import { getAllUser } from "./features/userSlice";
 import logo from "./logo.png";
 import { FeedPage } from "./pages";
 import { Landingpage } from "./pages/Landingpage/Landingpage";
 import { Signin } from "./pages/Signin/Signin";
 import { Signup } from "./pages/Signup/Signup";
 function App() {
+  const { token } = useSelector((state) => state.authentication);
+  const dispatch=useDispatch()
+  const {allUsers}=useSelector(state=>state.user)
+ 
+  useEffect(()=>{
+    if(token){
+      dispatch(getAllUser())
+    }
+    
+    
+  },[token,allUsers])
   return (
     <>
       <BrowserRouter>
